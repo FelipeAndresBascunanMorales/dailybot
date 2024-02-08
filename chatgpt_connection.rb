8707e77f96f28b875676bfe8cdc90fa28930950c
FelipeAndresBascunanMorales <f.bascunan03@gmail.com>
2024-02-07 23:50:02 -0300

    agrega algo

--- chatgpt_connection.rb
+++ chatgpt_connection.rb
@@ -17,11 +17,11 @@ class ChatgptConnection
     openai_url = "https://api.openai.com/v1/chat/completions"
 
     payload = {
-      "model": "gpt-4",
+      "model": "gpt-4-turbo-preview",
       "messages": [
         {
           "role": "system",
-          "content": "you will gonna receive an array with the data from tickets (in spanish). Your work is to return an json object with the following structure:\n[\n  {\n    id: Number,\n    title: String,\n    type: String[centra/bono/banco],\n    new_or_fix: String[nuevo/fix],\n    summary: String,\n  },\n  ...\n]\n\nattributes definition:\nid: is the same number of the ticket\ntitle: corresponds to the subject field, \n\ntype:\nthe most important attribute is the type, your work is to infers the type based in the data provided, the options are:\n\"centra\": when the ticket correspond to an accounting centralization (\"Centralización contable\" in Spanish) or accounting file.\n\"bono\": when the requirement is related to an settlement item\n\"banco\": when the ticket is related to an personalized format for a file that the bank will accept to generate payments or money transfers\n\nsummary: The summary attribute must be short, less that 6 words.\n\nnew_or_fix: that field must be \"nuevo\" for tickets that require something new or \"fix\" if in the tickets is required some kind of modification.\n"
+          "content": "you will gonna receive an array with the data from tickets (in spanish). Your work is to return an json object with the following structure:\n[\n  {\n    id: Number,\n    title: String,\n    type: String[centra/bono/banco],\n    new_or_fix: String[nuevo/fix],\n    company: String,\n    summary: String,\n  },\n  ...\n]\n\nattributes definition:\nid: is the same number of the ticket\ntitle: corresponds to the subject field, \n\ntype:\nthe most important attribute is the type, your work is to infers the type based in the data provided, the options are:\n\"centra\": when the ticket correspond to an accounting centralization (\"Centralización contable\" in Spanish) or accounting file.\n\"bono\": when the requirement is related to an settlement item\n\"banco\": when the ticket is related to an personalized format for a file that the bank will accept to generate payments or money transfers\n\ncompany: Is our client you can take the company name in the field custom_url_empresa or from the subdomain in the url subdomain.buk.[cl|pe|co|mx|br] \n\nsummary: The summary attribute must be short, less that 6 words.\n\nnew_or_fix: that field must be \"nuevo\" for tickets that require something new or \"fix\" if in the tickets is required some kind of modification.\n"
         },
         {
           "role": "user",
@@ -38,6 +38,7 @@ class ChatgptConnection
     JSON.parse(tickets)
     response = RestClient.post(openai_url, payload.to_json, {content_type: "application/json", :Authorization => "Bearer #{@api_key}"})
     content = JSON.parse(response.body)&.dig("choices",0, "message", "content")
+    content = content.gsub(/^```json\s*|```$/, '')
     JSON.parse(content)
   end
 end
